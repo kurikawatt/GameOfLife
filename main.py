d2060bac4847201f6f45774c81795a09ce4c98c5
François "Kurikawa <francois@kurikawa.fr>
2024-02-20 21:40:27 +0100

    flush() function

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 #!/bin/python
 # coding: utf-8
 
+import os
+import time
 from grid.Grid import Grid
+
+def flush():
+    """
+    Clean the term
+    """
+    if os.name == "nt": # for Windows users
+        os.system("cls")
+    else: # Unix-like OS
+        os.system("clear")
 
 if __name__ == "__main__":
     grid = Grid(10,10)
     print(grid)
+    time.sleep(1)
+    flush()
