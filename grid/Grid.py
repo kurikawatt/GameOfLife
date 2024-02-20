72344d342222c9b6e9a12e79c2a2a7efc14db923
François "Kurikawa <francois@kurikawa.fr>
2024-02-20 21:44:56 +0100

    randomly choose living or dead cell

--- grid/Grid.py
+++ grid/Grid.py
@@ -1,8 +1,16 @@
 # coding: utf-8
 
+import random as rdm
+
 class Grid:
     def __init__(self, length, width) -> None:
-        self.__content = [[0 for _ in range(length)] for _ in range(width)]
+        self.__length = length
+        self.__width = width
+        self.__content = [
+            [rdm.randint(0,1) # to randomly choose which cells live at begining 
+             for _ in range(length)] 
+            for _ in range(width)
+        ]
 
     def __str__(self) -> str:
         res:str = ""
